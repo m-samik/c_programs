97fcf9fa3812c58bfeba6e055d9e7b93cdf7a633
Muhammad Sami Khanday <muhammad.sami2706@gmail.com>
2020-12-10 14:03:25 +0530

    recoup classes programs

---
+++ recoup_programs/pattern_forloop.cpp
@@ -0,0 +1,11 @@
+//program for pattern
+#include<stdio.h>
+int main(){
+    int i,j;
+    for(i=0 ; i<3 ; i++)
+    {
+        for(j=0; j<=i ;j++)
+            printf("*");
+            printf("\n");
+    }
+}
